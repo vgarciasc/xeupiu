b687b1f307915ee866b0262fff5ad5e49bed0547
vgarcia <13024544+vgarciasc@users.noreply.github.com>
2024-09-15 09:59:55 -0300

    fixing bug where shinto shrine selection when alone was not detected for character selection textbox mapping

--- selectable_rect_overlay.py
+++ selectable_rect_overlay.py
@@ -227,11 +227,12 @@ def detect_mark_character_selection_choice_1(red, green, blue):
 def detect_mark_character_selection_choice_2(red, green, blue):
     is_players_house = detect_mark_by_count(red, green, blue, 252, 105, 29, 32, 173, 107, 82, 5)
     is_shinto_shrine = detect_mark_by_count(red, green, blue, 13, 90, 23, 27, 239, 222, 222, 64)
+    is_shinto_shrine_2 = detect_mark_by_count(red, green, blue, 17, 50, 21, 24, 74, 66, 58, 30)
     is_corridor_school = detect_mark_by_count(red, green, blue, 9, 20, 50, 90, 58, 74, 66, 24)
 
     is_selection_on_textbox = 30 < get_count_by_thresholds(red, green, blue, 16, 160, 280, 63, 60, 120, 160, 190, 20, 80) < 300
 
-    return (is_players_house or is_shinto_shrine or is_corridor_school) and is_selection_on_textbox
+    return (is_players_house or is_shinto_shrine or is_shinto_shrine_2 or is_corridor_school) and is_selection_on_textbox
 
 SCREEN_CUES = [
     { "id": "textbox", "fn": lambda r, g, b: detect_mark_by_count_min(r, g, b, 16, 159, 288, 64, 164, 49, 25, 650), "prerequisites": []},
